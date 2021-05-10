98b23b176e02383dcf651b3b333e02896f715ba9
hyejjun <45345124+hyejjun@users.noreply.github.com>
2021-05-10 16:37:00 +0900

    axios
    axios

--- server.js
+++ server.js
@@ -1,47 +1,18 @@
 const express = require('express');
-const {sequelize} = require('./models/index');    //객체
-const {User} = require('./models/index');          //User라는 변수만 가져오게됨
-const app = express();
-
-const router = require('./routers/index');
-const nunjucks = require('nunjucks');
-const bodyParser = require('body-parser');
-const session = require('express-session');     // npm install express-session
 const cors = require('cors');
-
+const app = express();
 
 app.use(cors());
 
+app.get('/',(req,res)=>{
+    //res.send('hello world');
+    res.json({
+        success:true,
+    });
+});
 
-app.set('view engine', 'html');
-nunjucks.configure('views',{
-    express:app,
-})
+app.listen(3001,()=>{
+    console.log('server 3001');
+});
 
-
-
-// 세션 설정값
-app.use(session({
-    secret : 'aaa',
-    resave : false,
-    saveUninitialized:true,
-}));
-
-app.use(bodyParser.urlencoded({extended:false}));
-
-
-sequelize.sync({ force : false, })
-.then(()=>{
-    console.log('접속 성공');
-})
-.catch(()=>{
-    console.log('접속 실패');
-})
-
-
-app.use('/',router);
-
-
-app.listen(3000,()=>{
-    console.log('server 3000');
-})
+// localhost:3000 부분 연결해서 쓸때는 이 server.js 가 아예 필요 없음
